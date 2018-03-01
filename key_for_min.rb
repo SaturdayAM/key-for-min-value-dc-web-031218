1a84c873d994adfc7d51e4f4a374d8a690e7e203
Jerry Lee <jwl62@georgetown.edu>
2018-03-01 06:37:21 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  to_return = ""
+  min = +1.0/0.0
+  name_hash.each do |key, value|
+    if value < min
+      min = value
+      to_return = key
+    end
+    to_return
 end
